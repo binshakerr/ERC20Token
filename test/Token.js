827decf41f169a68f561171991589e6b06efe61f
Eslam Shaker <binshakerr@gmail.com>
2023-01-23 22:48:33 +0200

    token test cases

---
+++ test/Token.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Token", () => {
+  let tokenSupply = "100";
+  let token;
+  let owner;
+  let addr1;
+  let addr2;
+
+  beforeEach(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy(tokenSupply);
+  });
+
+  describe("Deployment", () => {
+    it("should assign total supply of tokens to the owner/deployer", async () => {
+      const ownerBalance = await token.balanceOf(owner.address);
+      expect(await token.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", () => {
+    it("should transfer token between accounts", async () => {
+      await token.transfer(addr1.address, 50);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(50);
+    });
+
+    it("should not transfer token if it's more than account balance", async () => {
+      await expect(token.connect(addr1).transfer(addr2.address, 51)).to.be
+        .reverted;
+    });
+  });
+});
